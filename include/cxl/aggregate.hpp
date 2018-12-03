6e5413319efad416d2e6fa75f120e6601493f5e5
christian garcia <tacticalmelonfarmer@gmail.com>
2018-12-03 11:52:31 -0800

    rename example folder to examples following common practice
    made cxl::destructure<T>(T&&) support aggregate types of up to 24 members
    
    added lambda.hpp which will house the lambda sublibrary for manipulating, composing and applying lambda expressions

--- include/cxl/aggregate.hpp
+++ include/cxl/aggregate.hpp
@@ -31,11 +31,8 @@ struct max_size
 
 struct unknown_t
 {
-  template<typename T, typename = std::enable_if_t<!std::is_lvalue_reference<T>::value>>
-  operator T &&() const;
-
-  template<typename T, typename = std::enable_if_t<std::is_copy_constructible<T>::value>>
-  operator T&() const;
+  template<typename T>
+  operator T() const;
 };
 
 template<size_t N = 0>
@@ -96,76 +93,103 @@ destructure(T&& pod)
 
   if constexpr (arity == 1) {
     auto&& [v1] = pod;
-    return make_tuple<decltype(pod)>(v1);
+    return make_tuple<T&&>(v1);
   }
   if constexpr (arity == 2) {
     auto&& [v1, v2] = pod;
-    return make_tuple<decltype(pod)>(v1, v2);
+    return make_tuple<T&&>(v1, v2);
   }
   if constexpr (arity == 3) {
     auto&& [v1, v2, v3] = pod;
-    return make_tuple<decltype(pod)>(v1, v2, v3);
+    return make_tuple<T&&>(v1, v2, v3);
   }
   if constexpr (arity == 4) {
     auto&& [v1, v2, v3, v4] = pod;
-    return make_tuple<decltype(pod)>(v1, v2, v3, v4);
+    return make_tuple<T&&>(v1, v2, v3, v4);
   }
   if constexpr (arity == 5) {
     auto&& [v1, v2, v3, v4, v5] = pod;
-    return make_tuple<decltype(pod)>(v1, v2, v3, v4, v5);
+    return make_tuple<T&&>(v1, v2, v3, v4, v5);
   }
   if constexpr (arity == 6) {
     auto&& [v1, v2, v3, v4, v5, v6] = pod;
-    return make_tuple<decltype(pod)>(v1, v2, v3, v4, v5, v6);
+    return make_tuple<T&&>(v1, v2, v3, v4, v5, v6);
   }
-
   if constexpr (arity == 7) {
     auto&& [v1, v2, v3, v4, v5, v6, v7] = pod;
-    return make_tuple<decltype(pod)>(v1, v2, v3, v4, v5, v6, v7);
+    return make_tuple<T&&>(v1, v2, v3, v4, v5, v6, v7);
   }
   if constexpr (arity == 8) {
     auto&& [v1, v2, v3, v4, v5, v6, v7, v8] = pod;
-    return make_tuple<decltype(pod)>(v1, v2, v3, v4, v5, v6, v7, v8);
+    return make_tuple<T&&>(v1, v2, v3, v4, v5, v6, v7, v8);
   }
   if constexpr (arity == 9) {
     auto&& [v1, v2, v3, v4, v5, v6, v7, v8, v9] = pod;
-    return make_tuple<decltype(pod)>(v1, v2, v3, v4, v5, v6, v7, v8, v9);
+    return make_tuple<T&&>(v1, v2, v3, v4, v5, v6, v7, v8, v9);
   }
   if constexpr (arity == 10) {
     auto&& [v1, v2, v3, v4, v5, v6, v7, v8, v9, v10] = pod;
-    return make_tuple<decltype(pod)>(v1, v2, v3, v4, v5, v6, v7, v8, v9, v10);
+    return make_tuple<T&&>(v1, v2, v3, v4, v5, v6, v7, v8, v9, v10);
   }
   if constexpr (arity == 11) {
     auto&& [v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11] = pod;
-    return make_tuple<decltype(pod)>(v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11);
+    return make_tuple<T&&>(v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11);
   }
   if constexpr (arity == 12) {
     auto&& [v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12] = pod;
-    return make_tuple<decltype(pod)>(v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12);
+    return make_tuple<T&&>(v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12);
   }
   if constexpr (arity == 13) {
     auto&& [v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13] = pod;
-    return make_tuple<decltype(pod)>(v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13);
+    return make_tuple<T&&>(v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13);
   }
   if constexpr (arity == 14) {
     auto&& [v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14] = pod;
-    return make_tuple<decltype(pod)>(v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14);
+    return make_tuple<T&&>(v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14);
   }
   if constexpr (arity == 15) {
     auto&& [v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15] = pod;
-    return make_tuple<decltype(pod)>(v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15);
+    return make_tuple<T&&>(v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15);
   }
-  if constexpr (arity == 13) {
-    auto&& [v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13] = pod;
-    return make_tuple<decltype(pod)>(v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13);
+  if constexpr (arity == 16) {
+    auto&& [v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16] = pod;
+    return make_tuple<T&&>(v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16);
   }
-  if constexpr (arity == 14) {
-    auto&& [v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14] = pod;
-    return make_tuple<decltype(pod)>(v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14);
+  if constexpr (arity == 17) {
+    auto&& [v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17] = pod;
+    return make_tuple<T&&>(v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17);
   }
-  if constexpr (arity == 15) {
-    auto&& [v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15] = pod;
-    return make_tuple<decltype(pod)>(v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15);
+  if constexpr (arity == 18) {
+    auto&& [v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17, v18] = pod;
+    return make_tuple<T&&>(v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17, v18);
+  }
+  if constexpr (arity == 19) {
+    auto&& [v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17, v18, v19] = pod;
+    return make_tuple<T&&>(v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17, v18, v19);
+  }
+  if constexpr (arity == 20) {
+    auto&& [v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17, v18, v19, v20] = pod;
+    return make_tuple<T&&>(v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17, v18, v19, v20);
+  }
+  if constexpr (arity == 21) {
+    auto&& [v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17, v18, v19, v20, v21] = pod;
+    return make_tuple<T&&>(v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17, v18, v19, v20, v21);
+  }
+  if constexpr (arity == 22) {
+    auto&& [v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17, v18, v19, v20, v21, v22] = pod;
+    return make_tuple<T&&>(
+      v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17, v18, v19, v20, v21, v22);
+  }
+  if constexpr (arity == 23) {
+    auto&& [v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17, v18, v19, v20, v21, v22, v23] = pod;
+    return make_tuple<T&&>(
+      v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17, v18, v19, v20, v21, v22, v23);
+  }
+  if constexpr (arity == 24) {
+    auto&& [v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17, v18, v19, v20, v21, v22, v23, v24] =
+      pod;
+    return make_tuple<T&&>(
+      v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17, v18, v19, v20, v21, v22, v23, v24);
   }
 }
 }
---
+++ include/cxl/lambda.hpp
@@ -0,0 +1,59 @@
+#pragma once
+#include <new>
+#include <type_traits>
+#include <utility>
+
+template<typename Return, typename... Parameters>
+struct Iwrap_lambda
+{
+  virtual ~Iwrap_lambda() {}
+  virtual Return operator()(Parameters&&...) = 0;
+  virtual Return operator()(Parameters&&...) const = 0;
+};
+
+template<typename Lambda, typename Return, typename... Parameters>
+struct wrap_lambda final : public Iwrap_lambda<Return, Parameters...>
+{
+  wrap_lambda(Lambda lambda)
+    : lambda_(std::move(lambda))
+  {}
+
+  Return operator()(Parameters&&... arguments) override { return lambda_(std::forward<Parameters>(arguments)...); }
+
+  Return operator()(Parameters&&... arguments) const override
+  {
+    return lambda_(std::forward<Parameters>(arguments)...);
+  }
+
+private:
+  Lambda lambda_;
+};
+
+namespace lambda {
+template<typename CallSign>
+struct wrap;
+
+template<typename Return, typename... Parameters>
+struct wrap<Return(Parameters...)>
+{
+  template<typename Lambda>
+  wrap(Lambda&& lambda)
+  {
+    new (access()) wrap_lambda<Lambda, Return, Parameters...>(std::forward<Lambda>(lambda));
+  }
+
+  ~wrap() { access()->~Iwrap_lambda<Return, Parameters...>(); }
+
+  Return operator()(Parameters&&... arguments) { return access()->operator()(std::forward<Parameters>(arguments)...); }
+
+  Return operator()(Parameters&&... arguments) const
+  {
+    return access()->operator()(std::forward<Parameters>(arguments)...);
+  }
+
+private:
+  auto access() { return reinterpret_cast<Iwrap_lambda<Return, Parameters...>*>(&wrapper_); }
+  auto access() const { return reinterpret_cast<const Iwrap_lambda<Return, Parameters...>*>(&wrapper_); }
+  std::aligned_storage_t<8> wrapper_;
+};
+}
