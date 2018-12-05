a15424fdc45fac5b429838fe651dab8019299308
christian garcia <tacticalmelonfarmer@gmail.com>
2018-12-05 01:51:07 -0800

    added constructor and destructor emulators in the lambda sublibrary and rename fake_class example to component

---
+++ examples/component/main.cpp
@@ -0,0 +1,51 @@
+#include <cxl/aggregate.hpp>
+#include <cxl/lambda.hpp>
+#include <iostream>
+
+struct statistic
+{
+  int sum, data_points;
+};
+
+struct component
+{
+  statistic value;
+  cxl::lambda::wrap<int(int)> method;
+};
+
+struct managed_component
+{
+  statistic value;
+  cxl::lambda::wrap<int(int)> method;
+  cxl::lambda::wrap<void()> constructor;
+  cxl::lambda::wrap<void()> destructor;
+};
+
+int
+main()
+{
+  using std::get;
+
+  component accumulator{ statistic{ 0, 0 }, [&](auto input) {
+                          accumulator.value.sum += input;
+                          return accumulator.value.sum;
+                        } };
+
+  component average{ statistic{ 0, 0 }, [&](auto input) {
+                      auto& stat = average.value;
+                      stat.sum += input;
+                      stat.data_points += 1;
+                      return stat.sum / stat.data_points;
+                    } };
+
+  managed_component config{ statistic{ 0, 0 },
+                            [&](auto input) { return 0; },
+                            cxl::lambda::ctor([&] {
+                              config.value.sum = 4;
+                              config.value.data_points = 20;
+                              std::cout << "hello!\n";
+                            }),
+                            cxl::lambda::dtor([&] { std::cout << "goodbye!\n"; }) };
+
+  return 0;
+}
--- include/cxl/lambda.hpp
+++ include/cxl/lambda.hpp
@@ -7,6 +7,10 @@ namespace cxl {
 
 namespace lambda {
 
+namespace error {
+struct uninitialized_lambda_call
+{};
+}
 inline namespace detail {
 
 template<typename Return, typename... Parameters>
@@ -26,10 +30,7 @@ struct wrap_lambda final : public Iwrap_lambda<Return, Parameters...>
 
   Return operator()(Parameters... arguments) override { return lambda_(arguments...); }
 
-  Return operator()(Parameters... arguments) const override
-  {
-    return lambda_(arguments...);
-  }
+  Return operator()(Parameters... arguments) const override { return lambda_(arguments...); }
 
 private:
   Lambda lambda_;
@@ -42,20 +43,37 @@ struct wrap;
 template<typename Return, typename... Parameters>
 struct wrap<Return(Parameters...)>
 {
+  wrap()
+    : empty_(true)
+  {}
+
   template<typename Lambda>
   wrap(Lambda&& lambda)
   {
-    new (access()) wrap_lambda<Lambda, Return, Parameters...>(std::forward<Lambda>(lambda));
+    auto ptr = access();
+    new (ptr) wrap_lambda<Lambda, Return, Parameters...>(std::forward<Lambda>(lambda));
+    empty_ = false;
   }
 
-  ~wrap() { access()->~Iwrap_lambda<Return, Parameters...>(); }
+  ~wrap()
+  {
+    if (!empty_)
+      access()->~Iwrap_lambda<Return, Parameters...>();
+  }
 
   template<typename... UParameters>
-  Return operator()(UParameters&&... arguments) { return access()->operator()(std::forward<Parameters>(arguments)...); }
+  Return operator()(UParameters&&... arguments)
+  {
+    if (empty_)
+      throw error::uninitialized_lambda_call{};
+    return access()->operator()(std::forward<Parameters>(arguments)...);
+  }
 
   template<typename... UParameters>
   Return operator()(UParameters&&... arguments) const
   {
+    if (empty_)
+      throw error::uninitialized_lambda_call{};
     return access()->operator()(std::forward<Parameters>(arguments)...);
   }
 
@@ -63,6 +81,30 @@ private:
   auto access() { return reinterpret_cast<Iwrap_lambda<Return, Parameters...>*>(&wrapper_); }
   auto access() const { return reinterpret_cast<const Iwrap_lambda<Return, Parameters...>*>(&wrapper_); }
   std::aligned_storage_t<8> wrapper_;
+  bool empty_;
 };
+
+template<typename Lambda>
+constexpr decltype(auto)
+ctor(Lambda&& lambda)
+{
+  lambda();
+  return (lambda);
+}
+
+template<typename Lambda>
+constexpr auto&&
+dtor(Lambda&& lambda)
+{
+  struct local_t : Lambda
+  {
+    local_t(Lambda&& _)
+      : Lambda(_)
+    {}
+    using Lambda::operator();
+    ~local_t() { (*this)(); }
+  } local(std::forward<Lambda>(lambda));
+  return local;
+}
 }
 }
